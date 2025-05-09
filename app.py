7b3ca121e634dde746f54a9fd7ab2d68074249db
Abhonotik <abhonotik14@gmail.com>
2025-05-09 13:37:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,16 +4,16 @@ import pickle
 import streamlit as st
 import pandas as pd
 
-# Step 1: Extract the ZIP file only if simmilarity.pkl doesn't exist
-if not os.path.exists('simmilarity.pkl'):
-    with zipfile.ZipFile('simmilarity.zip', 'r') as zip_ref:
-        zip_ref.extractall()
+# Step 1: Extract the ZIP file only if similarity.pkl doesn't exist
+if not os.path.exists('data/similarity.pkl'):
+    with zipfile.ZipFile('data/similarity.zip', 'r') as zip_ref:
+        zip_ref.extractall('data')
 
 # Step 2: Load similarity matrix and movie dictionary
-with open('simmilarity.pkl', 'rb') as f:
-    simmilarity = pickle.load(f)
+with open('data/similarity.pkl', 'rb') as f:
+    similarity = pickle.load(f)
 
-with open('movie_dict.pkl', 'rb') as f:
+with open('data/movie_dict.pkl', 'rb') as f:
     movies_dict = pickle.load(f)
 
 movies = pd.DataFrame(movies_dict)
@@ -21,7 +21,7 @@ movies = pd.DataFrame(movies_dict)
 # Step 3: Define the recommendation function
 def recommend(movie_title):
     movie_index = movies[movies["title"] == movie_title].index[0]
-    distances = simmilarity[movie_index]
+    distances = similarity[movie_index]
     movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
 
     recommended_movies = []
